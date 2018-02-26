a33f5def5c79f8c28d686e8174058a2587735a73
Bruno Silva <brunnoborges@ufrn.edu.br>
2018-02-26 17:10:23 -0300

    Add sql

---
+++ sql/createtables.sql
@@ -0,0 +1,26 @@
+create schema SGIP;
+use SGIP;
+set charset utf8;
+
+/*
+ * Autores:
+ * 
+ * Alcindo Júnior
+ * Bruno Borges
+ * Isaac José
+ * Joaquim Heitor
+ * Vanderson Fábio
+ * 
+*/
+
+create table patente(
+	id int(9) not null auto_increment
+);
+
+create table oficial(
+	id int(9) not null auto_increment
+);
+
+create table permuta(
+	id int(9) not null auto_increment
+);
